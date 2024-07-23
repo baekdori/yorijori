5c2772e50875790df3b9d0afe4a826fc0bc5f01f
백종호 <155928591+baekdori@users.noreply.github.com>
2024-07-23 16:03:58 +0900

    App.js에서 리액트 기본 로고 삭제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +16,10 @@ function App() {
         >
           Learn React
         </a>
+        <MainPage />
       </header>
     </div>
   );
-} //rtrthfhrertrwerewr
+}
 
 export default App;
