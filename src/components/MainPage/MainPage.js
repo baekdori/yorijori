1301848546a91df303baebcd519efbcf26529012
백종호 <155928591+baekdori@users.noreply.github.com>
2024-07-23 17:44:40 +0900

    메인페이지 틀 구현(키워드 검색, 비주얼 검색 클릭 구현, 추천 메뉴 클릭 구현)

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -2,6 +2,18 @@ import React from 'react';
 import './MainPage.css';
 
 const MainPage = () => {
+  // 클릭 시 호출될 함수
+  const keywordSearching = () => {
+    console.log("키워드 검색 실행!");
+    // 키워드 검색 관련 로직을 여기에 추가
+  };
+
+  const visualSearching = () => {
+    console.log("비주얼 검색 실행!");
+    // 비주얼 검색 관련 로직을 여기에 추가
+  };
+
+  // 레시피 결과 보기 버튼 클릭 핸들러
   const handleClick = () => {
     try {
       window.location.href = '/ResultPage';
@@ -13,16 +25,21 @@ const MainPage = () => {
 
   return (
     <div className="main-page">
-      <div className="food-pic">
-        <div className="recipe-text">레시피 찾아보기</div>
-        <div className="select-btn-container">
-          <div className="left-container">
-            <div className="searching-input"></div>
+      <div className="recipe-text">레시피 찾아보기</div>
+      <div className="select-btn-container">
+        <div className="left-container" onClick={keywordSearching}>
+          <div className="searching-input">
+            <div className="searching-input-text">키워드 검색</div>
           </div>
-          <div className="tiktok"></div>
-          <div className="right-container"></div>
+        </div>
+        <div className="tiktok"></div>
+        <div className="right-container" onClick={visualSearching}>
+          <div className="searching-plate">
+            <div className="searching-plate-text">비주얼 검색</div>
+          </div>
         </div>
       </div>
+      <div className="food-pic"></div>
       <div className="recom-container">
         <div className="recom-text">OO님이 좋아할 요리를 찾았어요!</div>
         <div className="recom-subtext">OO님의 기록을 분석하여 찾은 결과입니다</div>
@@ -30,8 +47,10 @@ const MainPage = () => {
       <div className="food-result-top-container">
         <div className="fr" onClick={handleClick}></div>
         <div className="fr" onClick={handleClick}></div>
+        <div className="fr" onClick={handleClick}></div>
       </div>
       <div className="food-result-bottom-container">
+        <div className="fr" onClick={handleClick}></div>
         <div className="fr" onClick={handleClick}></div>
         <div className="fr" onClick={handleClick}></div>
       </div>
