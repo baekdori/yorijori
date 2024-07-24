b4977607c56d37cd8514564c09cee9826213919e
chaeliwon <danyub987@gmail.com>
2024-07-24 10:24:58 +0900

    app.js에 로그인 회원가입 리액트 import할수있게 수정되었습니다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react'; // 일단 gpt가 넣어서 넣어놨는데 문제시 삭제예정
 import './App.css';
 import MainPage from './components/MainPage/MainPage';
+import SignupPage from './components/SignupPage/SignupPage';  // SignupPage 컴포넌트 import
+import LoginPage from './components/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-        <MainPage />
+      <MainPage />
+      <SignupPage />  {/* SignupPage 컴포넌트 사용 */}
+      <LoginPage /> {/* LoginPage 컴포넌트 사용 */}
     </div>
   );
 }
---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { DivWrapper } from '../../components/DivWrapper';
+import { XnixLineStar } from '../../components/XnixLineStar';
+import { XnixLineSearch4 } from '../../icons/XnixLineSearch4';
+import { XnixLineStar10 } from '../../icons/XnixLineStar10';
+import './LoginPage.css';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      {/* 필요한 컴포넌트 사용 */}
+    </div>
+  );
+};
+
+export default LoginPage;
