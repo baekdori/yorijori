2ec33a15bbb248f89ab5d581a3a1538acb08ebce
gonaeun <gonaeunn@gmail.com>
2024-07-25 15:01:34 +0900

    마이페이지, 업데이트라우터

---
+++ server/routes/UpdateRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../model/user');
+
+// 하은 내용 수정할 예정 : 회원정보 수정 요청을 처리
+
+
+model.exports = router;
--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -1,75 +1,9 @@
 import React from 'react';
-import axios from 'axios'
 import './MyPage.css';
 
+// 하은 4000포트로 보내는 코드입력할예정
+
 const MyPage = () => {
-  const [formData, setFormData] = useState({    // useState 훅을 사용하여 formData 상태 정의
-    userId: '',
-    nickname: '',
-    phone: '',
-    email: '',
-    joinDate: ''
-  });
-
-  // 페이지 로드 시 사용자 정보 불러오기
-  // useEffect 훅을 사용하여 컴포넌트가 처음 렌더링 될 때 fetchUserData 함수 호출 
-  useEffect(() => {
-    const userId = 'testUser'; // 예시로 사용자 ID를 설정
-    fetchUserData(userId);
-  }, []);
-
-  const fetchUserData = async (userId) => {
-    try {
-      const response = await axios.get(`http://localhost:4000/user/${userId}`);
-      setFormData({
-        userId: response.data.user_id,
-        nickname: response.data.user_nick,
-        phone: response.data.user_phone,
-        email: response.data.user_email,
-        joinDate: response.data.joined_at
-      });
-    } catch (error) {
-      console.error('사용자 정보 불러오기 오류:', error);
-    }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.put(`http://localhost:4000/user/mypage/${formData.userId}`, {
-        user_nick: formData.nickname,
-        user_phone: formData.phone,
-        user_email: formData.email
-      });
-      if (response.status === 200) {
-        alert('회원정보가 성공적으로 수정되었습니다.');
-      } else {
-        alert('회원정보 수정 실패');
-      }
-    } catch (error) {
-      console.error('회원정보 수정 중 오류 발생:', error);
-      alert('회원정보 수정 도중 오류 발생');
-    }
-  };
-
-
-
-
-
-
-
-
-
-
-
   return (
     <div className="mypage">
       <div className="title">마이페이지</div>
