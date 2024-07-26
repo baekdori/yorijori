28acef687aabc9dd24b65c0292e06f50b88605ff
chaeliwon <danyub987@gmail.com>
2024-07-26 16:48:19 +0900

    하단바!

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import './BottomBar.css';
+import './test.css';
 
-const BottomBar = () => {
-    return (
-        <div className="bottom-nav">
-            <button className="nav-item">
-                <img className="Category" alt="Category" src="/static/img/puzzle.png" />
-            </button>
-            <button className="nav-item">
-            <img className="Post" alt="Post" src="/static/img/puzzle.png" />
-            </button>
-            <button className="nav-item">
-            <img className="Home" alt="Home" src="/static/img/puzzle.png" />
-            </button>
-            <button className="nav-item">
-            <img className="Like" alt="Like" src="/static/img/puzzle.png" />
-            </button>
-            <button className="nav-item">
-            <img className="My Page" alt="My Page" src="/static/img/puzzle.png" />
-            </button>
-        </div>
-    );
+const BottomNav = () => {
+  return (
+    <div className="bottom-nav">
+      <a href="/category" className="nav-item">
+        <img className="Category" alt="Category" src="/static/img/puzzle.png" />
+        <span>카테고리</span>
+      </a>
+      <a href="/post" className="nav-item">
+        <img className="Post" alt="Post" src="/static/img/message.png" />
+        <span>게시글작성</span>
+      </a>
+      <a href="/home" className="nav-item">
+        <img className="Home" alt="Home" src="/static/img/home.png" />
+        <span>홈</span>
+      </a>
+      <a href="/like" className="nav-item">
+        <img className="Like" alt="Like" src="/static/img/heart.png" />
+        <span>좋아요</span>
+      </a>
+      <a href="/mypage" className="nav-item">
+        <img className="MyPage" alt="MyPage" src="/static/img/users.png" />
+        <span>내 정보</span>
+      </a>
+    </div>
+  );
 };
 
-export default BottomBar;
+export default BottomNav;
