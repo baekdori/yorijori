6f3fd44001e6ed063ea6caad83f497835f159c67
chaeliwon <danyub987@gmail.com>
2024-07-26 11:51:00 +0900

    경로 오류

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/mypage" element={<MyPage />} />
+          {/* <Route path="/mypage" element={<MyPage />} /> */}
         </Routes>
       </div>
     </Router>
