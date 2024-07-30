cc50b168c74da983541389738cd67877d3548921
chaeliwon <danyub987@gmail.com>
2024-07-30 17:02:54 +0900

    마이페이지 js, css 수정

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './MyPage.css';
+import TopBar from '../TopBar/TopBar.js';
+import BottomBar from '../BottomBar/BottomBar.js';
 // import { useNavigate } from 'react-router-dom'; // useNavigate 훅 가져오기
 
 const MyPage = () => {
@@ -8,7 +10,7 @@ const MyPage = () => {
 
   // 사용자 데이터 상태와 수정가능여부 관리하는 상태를 정의
   const [userData, setUserData] = useState({
-    user_id: '', 
+    user_id: '',
     user_nick: '',
     user_phone: '',
     user_email: ''
@@ -46,7 +48,7 @@ const MyPage = () => {
     try {
       console.log('회원정보 수정 요청 데이터:', userData);
       // 백엔드 서버(4000)로 POST 요청보내서 수정된 사용자 데이터 전송
-      const response = await axios.put("http://localhost:4000/user/mypage", userData, { withCredentials: true }); 
+      const response = await axios.put("http://localhost:4000/user/mypage", userData, { withCredentials: true });
       console.log('서버 응답 데이터:', response.data);
       if (response.status === 200) {
         setIsEditing(false);  // 서버로부터 성공 응답 받으면, 수정모드를 비활성화
@@ -62,36 +64,39 @@ const MyPage = () => {
     }
   };
 
-    // 회원 탈퇴 버튼 클릭
-    const handleDeleteClick = async () => {
-      const confirmDelete = window.confirm('정말로 회원 탈퇴를 하시겠습니까?');
-      if (!confirmDelete) return;
+  // 회원 탈퇴 버튼 클릭
+  const handleDeleteClick = async () => {
+    const confirmDelete = window.confirm('정말로 회원 탈퇴를 하시겠습니까?');
+    if (!confirmDelete) return;
 
-      try {
-        // 백엔드 서버(4000)로 요청 보내서 사용자 데이터 삭제
-        // 프론트에서 탈퇴 요청을 보낼때, url경로에 사용자 ID를 포함시킴 (사용자ID를 명시적으로 전달하여, 경로를 통해 특정 사용자를 식별,삭제 가능) 
-        console.log('회원 탈퇴 요청 전송:', userData.user_id);
-        const response = await axios.delete(`http://localhost:4000/user/mypage/${userData.user_id}`, { withCredentials: true });
-        console.log('서버 응답 데이터:', response.data);
-        if (response.status === 200) {
-          alert('회원 탈퇴가 성공적으로 완료되었습니다.');
-          console.log('회원 탈퇴 응답:', response.data);
-          // navigate('/login'); 
-          window.location.href="/login"  // 탈퇴 후 로그인 페이지로 이동
-        } else {
-          alert('회원 탈퇴 실패');
-          console.error('회원 탈퇴 실패:', response);
-        }
-      } catch (error) {
-        console.error('회원 탈퇴 중 오류 발생:', error);
-        alert('회원 탈퇴 도중 오류 발생');
+    try {
+      // 백엔드 서버(4000)로 요청 보내서 사용자 데이터 삭제
+      // 프론트에서 탈퇴 요청을 보낼때, url경로에 사용자 ID를 포함시킴 (사용자ID를 명시적으로 전달하여, 경로를 통해 특정 사용자를 식별,삭제 가능) 
+      console.log('회원 탈퇴 요청 전송:', userData.user_id);
+      const response = await axios.delete(`http://localhost:4000/user/mypage/${userData.user_id}`, { withCredentials: true });
+      console.log('서버 응답 데이터:', response.data);
+      if (response.status === 200) {
+        alert('회원 탈퇴가 성공적으로 완료되었습니다.');
+        console.log('회원 탈퇴 응답:', response.data);
+        // navigate('/login'); 
+        window.location.href = "/login"  // 탈퇴 후 로그인 페이지로 이동
+      } else {
+        alert('회원 탈퇴 실패');
+        console.error('회원 탈퇴 실패:', response);
       }
-    };
+    } catch (error) {
+      console.error('회원 탈퇴 중 오류 발생:', error);
+      alert('회원 탈퇴 도중 오류 발생');
+    }
+  };
 
   return (
     <div className="mypage">
+      <TopBar />
       <div className="title">마이페이지</div>
+
       <div className="first-line"></div>
+
       <div className="id-container">
         <div className="picture-circle"></div>
         <div className="id-info">
@@ -101,20 +106,23 @@ const MyPage = () => {
           <div className="input-prompt">이름</div>
         </div>
       </div>
+
       <div className="second-line"></div>
+
       <div className="nickname">닉네임</div>
       <input
-        type="text" /* 수정 버튼을 눌러야 input 칸 선택 가능 */
+        type="text"
         className="nick-input"
         name="user_nick"
         value={userData.user_nick}
         onChange={handleInputChange}
         readOnly={!isEditing}
         style={{ userSelect: isEditing ? 'auto' : 'none' }}
-      /> 
-      <div className="phone">핸드폰</div> 
+      />
+
+      <div className="phone">핸드폰</div>
       <input
-        type="text" /* 수정 버튼을 눌러야 input 칸 선택 가능 */
+        type="text"
         className="phone-input"
         name="user_phone"
         value={userData.user_phone}
@@ -122,9 +130,10 @@ const MyPage = () => {
         readOnly={!isEditing}
         style={{ userSelect: isEditing ? 'auto' : 'none' }}
       />
+
       <div className="email">이메일</div>
       <input
-        type="text" /* 수정 버튼을 눌러야 input 칸 선택 가능 */
+        type="text"
         className="email-input"
         name="user_email"
         value={userData.user_email}
@@ -132,13 +141,15 @@ const MyPage = () => {
         readOnly={!isEditing}
         style={{ userSelect: isEditing ? 'auto' : 'none' }}
       />
-      <button className="correction-btn" onClick={handleCorrectionClick}>수정</button>
-      <button className="submit-btn" onClick={handleSubmitClick} disabled={!isEditing}>확인</button>
-      
-      {/* 회원 탈퇴 버튼 */}
+
       <button className="delete-account-btn" onClick={handleDeleteClick}>
         회원 탈퇴
       </button>
+
+      <BottomBar />
+
+      <button className="correction-btn" onClick={handleCorrectionClick}>수정</button>
+      <button className="submit-btn" onClick={handleSubmitClick} disabled={!isEditing}>확인</button>
     </div>
   );
 };
