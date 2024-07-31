245e9887880ea6bb7f9ba72f5299d9e48df79ee6
Your Name <you@example.com>
2024-07-31 10:49:11 +0900

    검색데이터 디테일이동

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './components/MainPage/MainPage';
@@ -10,18 +10,21 @@ import TopBar from './components/TopBar/TopBar';
 import RecipeAddPage from './components/RecipeAddPage/RecipeAddPage';
 
 function App() {
+  const [selectedResult, setSelectedResult] = useState(null);
+  console.log('app.js로 가져온 상세보기 결과',selectedResult)
+
   return (
     <Router>
       <div className="App">
        <TopBar /> {/* 공통 컴포넌트 TopBar 추가 */}
         <Routes>
           {/* 라우팅 설정  */}
-          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<MainPage setSelectedResult={setSelectedResult}/>} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/DetailPage" element={<DetailPage />} />
-          <Route path="/RecipeAddPage" element={<RecipeAddPage />} />
+          <Route path="/DetailPage" element={<DetailPage result={selectedResult}/>} />
         </Routes>
       </div>
     </Router>
