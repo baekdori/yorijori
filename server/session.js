379899b23bd8d5402089a3333c8699ed63cc9841
백종호 <155928591+baekdori@users.noreply.github.com>
2024-10-09 23:12:13 +0900

    rolling : true를 통해 요청이 발생하면 세션 쿠키 만료 시간을 갱신
    maxAge : null로 세션 쿠기가 브라우저 창이 닫히면 만료

--- server/session.js
+++ server/session.js
@@ -11,12 +11,14 @@ const fileStoreOptions = {
 module.exports = {
   sessionMiddleware: session({
     secret: 'Hexacore6!!',
-    resave: false,
-    saveUninitialized: false, // 초기화되지 않은 세션을 저장 안 함
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000,  // 24시간 유지 (또는 원하는 시간으로 설정)
-      secure: false // 개발 환경에서는 secure 설정 해제
-    },
+    resave: false,             // 세션을 강제로 저장할지 여부
+    saveUninitialized: false,   // 초기화되지 않은 세션을 저장소에 저장할지 여부
+    rolling : true, // 요청이 발생하면 세션 쿠키 만료 시간을 갱신
+    cookie : {
+    maxAge : null, // 세션 쿠기가 브라우저 창이 닫히면 만료
+    secure: false,        // 개발 환경에서는 false로 설정 (HTTPS가 아니기 때문에)
+    httpOnly: true        // 클라이언트 자바스크립트에서 쿠키 접근 불가
+  },
     store: new FileStore(fileStoreOptions)
   })
 };
